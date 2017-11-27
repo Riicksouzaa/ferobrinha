d2bcdb8abbfcd9a355c902d78215037485be234c
Marco Oliveira <marcomoa@hotmail.com>
2017-11-27 18:13:09 -0200

    Typo

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -5,7 +5,7 @@ if(!defined('INITIALIZED'))
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Tibia - Free Multiplayer Online Role Playing Game</title>
+    <title>Ferobra - Free Multiplayer Online Role Playing Game</title>
     <meta name="author" content="Marco Oliveira and Felipe Monteiro">
     <meta http-equiv="content-language" content="pt-br">
     <meta name="keywords" content="free online game, free multiplayer game, free online rpg, free mmorpg, mmorpg, mmog, online role playing game, online multiplayer game, internet game, online rpg, rpg">
