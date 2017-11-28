688c2051304a04a65b7b0bbea1815007dda5f1fc
Ricardo Souza <souzaariick@gmail.com>
2017-11-27 23:56:46 -0200

    need test

--- system/load.compat.php
+++ system/load.compat.php
@@ -254,7 +254,7 @@ if(!ONLY_PAGE)
 	{
 		$config['status']['serverStatus_checkInterval'] = $statustimeout+3;
 		$config['status']['serverStatus_lastCheck'] = time();
-		$statusInfo = new ServerStatus($config['server']['ip'], $config['server']['statusProtocolPort'], 1);
+		$statusInfo = new ServerStatus($config['server']['ip'], $config['server']['statusProtocolPort']);
 		if($statusInfo->isOnline())
 		{
 			$config['status']['serverStatus_online'] = 1;
