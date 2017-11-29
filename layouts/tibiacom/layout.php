af1f951ad676fa6fdc5b9e78d7ef3e98846876b7
Marco Oliveira <marcomoa@hotmail.com>
2017-11-29 21:30:23 -0200

    Upload ticket support

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -409,6 +409,16 @@ if(!defined('INITIALIZED'))
                                         <div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
                                     </div>
                                 </a>
+							<?php if (isset($_SESSION['account'])){?>
+							    <a href="?subtopic=ticket">
+                                    <div id="submenu_ticket" class="Submenuitem" onmouseover="MouseOverSubmenuItem(this)" onmouseout="MouseOutSubmenuItem(this)">
+                                        <div class="LeftChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
+                                        <div id="ActiveSubmenuItemIcon_ticket" class="ActiveSubmenuItemIcon" style="background-image:url(<?php echo $layout_name; ?>/images/global/menu/icon-activesubmenu.gif);"></div>
+                                        <div id="ActiveSubmenuItemLabel_ticket" class="SubmenuitemLabel">Ticket Support</div>
+                                        <div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
+                                    </div>
+                                </a>
+							<?php } ?>
                             </div>
                         </div>
                         <div id="shop" class="menuitem">
