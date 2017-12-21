d9b512993c8cc7ad368161d890d4cd0037139316
Marco Oliveira <marcomoa@hotmail.com>
2017-12-21 21:02:05 -0200

    Removendo changelog

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -185,14 +185,6 @@ if(!defined('INITIALIZED'))
                                         <div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
                                     </div>
                                 </a>
-								<a href="?subtopic=changelog">
-  									<div id="submenu_changelog" class="Submenuitem" onmouseover="MouseOverSubmenuItem(this)" onmouseout="MouseOutSubmenuItem(this)">
-    									<div class="LeftChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
-										<div id="ActiveSubmenuItemIcon_changelog" class="ActiveSubmenuItemIcon" style="background-image:url(<?php echo $layout_name; ?>/images/global/menu/icon-activesubmenu.gif);"></div>
-										<div id="ActiveSubmenuItemLabel_changelog" class="SubmenuitemLabel">Changelog</div>
-										<div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
-  									</div>
-								</a>
                             </div>
                         </div>
 
