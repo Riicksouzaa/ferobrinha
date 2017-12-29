82f9a93cf73bf5793ed18d0925041557a2688757
Ricardo Souza <souzaariick@gmail.com>
2017-12-29 16:48:53 -0200

    fix js of new page

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -420,6 +420,16 @@ if(!defined('INITIALIZED'))
                                         <div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
                                     </div>
                                 </a>
+                            <?php if($_REQUEST["subtopic"] == "erro"){?>
+                                <a href="?subtopic=erro">
+                                    <div id="submenu_erro" class="Submenuitem" onmouseover="MouseOverSubmenuItem(this)" onmouseout="MouseOutSubmenuItem(this)">
+                                        <div class="LeftChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
+                                        <div id="ActiveSubmenuItemIcon_ticket" class="ActiveSubmenuItemIcon" style="background-image:url(<?php echo $layout_name; ?>/images/global/menu/icon-activesubmenu.gif);"></div>
+                                        <div id="ActiveSubmenuItemLabel_ticket" class="SubmenuitemLabel">Erro</div>
+                                        <div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
+                                    </div>
+                                </a>
+                            <?}?>
 							<?php if (isset($_SESSION['account'])){?>
 							    <a href="?subtopic=ticket">
                                     <div id="submenu_ticket" class="Submenuitem" onmouseover="MouseOverSubmenuItem(this)" onmouseout="MouseOutSubmenuItem(this)">
@@ -480,7 +490,7 @@ if(!defined('INITIALIZED'))
                                         <div id="submenu_tankyou" class="Submenuitem" onmouseover="MouseOverSubmenuItem(this)" onmouseout="MouseOutSubmenuItem(this)">
                                             <div class="LeftChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
                                             <div id="ActiveSubmenuItemIcon_tankyou" class="ActiveSubmenuItemIcon" style="background-image:url(<?php echo $layout_name; ?>/images/global/menu/icon-activesubmenu.gif);"></div>
-                                            <div id="ActiveSubmenuItemLabel_tankyou" class="SubmenuitemLabel">Tankyou</div>
+                                            <div id="ActiveSubmenuItemLabel_tankyou" class="SubmenuitemLabel">Thank You!</div>
                                             <div class="RightChain" style="background-image:url(<?php echo $layout_name; ?>/images/global/general/chain.gif);"></div>
                                         </div>
                                     </a>
