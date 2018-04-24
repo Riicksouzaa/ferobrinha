afcd6cb961452c9d0f3986c03d9e5a4973bf3f4e
Ricardo Souza <souzaariick@gmail.com>
2018-04-23 21:39:40 -0300

    double status implement

--- send_payment_voucher.php
+++ send_payment_voucher.php
@@ -167,8 +167,6 @@ if ($config['site']['send_emails']) {
         $mail->Body = $newMailBody;
 //                        $mail->AltBody = $mailBody;
         $mail->send();
-        
-        echo 'Message has been sent. ';
     } catch (Exception $e) {
         echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
     }
