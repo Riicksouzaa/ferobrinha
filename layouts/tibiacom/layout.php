9091eebd0d37b16615e84b70dd13315702fa0b9b
Ricardo Souza <souzaariick@gmail.com>
2018-04-25 13:19:42 -0300

    htacess and retirado > irritante

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -15,7 +15,7 @@ if(!defined('INITIALIZED'))
     <?php if($_REQUEST['subtopic'] == "worlds"){$ch = (isset($_REQUEST['world']) ? $_REQUEST['world'] : null);}?>
     <?php if($_REQUEST['subtopic'] == "highscores"){$ch = (isset($_REQUEST['profession']) ? $highscores_list[$_REQUEST['list']]." -> ".$vocations_list[$_REQUEST['profession']].($_REQUEST['profession']>0?($_REQUEST['profession']<10?"s":null):null) : "Experience Points -> ALL");}?>
     <?php if($_REQUEST['subtopic'] == "houses"){$ch = (isset($_REQUEST['town']) ? $towns_list[$_REQUEST['town']] : (isset($_REQUEST['show']) ? $_REQUEST['show'] : null));}?>
-    <title><?=$config['server']['serverName'].(isset($_REQUEST['subtopic'])? " -> ".ucfirst($_REQUEST['subtopic']) :'').(isset($_REQUEST['action'])?" -> ".ucfirst($_REQUEST['action']):"").(isset($ch)?" -> ".ucfirst($ch):"")?> -> Free Multiplayer Online Role Playing Game</title>
+    <title><?=$config['server']['serverName'].(isset($_REQUEST['subtopic'])? " - ".ucfirst($_REQUEST['subtopic']) :'').(isset($_REQUEST['action'])?" - ".ucfirst($_REQUEST['action']):"").(isset($ch)?" - ".ucfirst($ch):"")?> - Free Multiplayer Online Role Playing Game</title>
     <meta name="author" content="Ricardo Souza - Codenome">
     <meta http-equiv="content-language" content="pt-br">
     <meta name="keywords" content="free online game, free multiplayer game, free online rpg, free mmorpg, mmorpg, mmog,
--- pages/accountmanagement/sellchar-util/insertAccount.sql
+++ pages/accountmanagement/sellchar-util/insertAccount.sql
@@ -1,9 +1,9 @@
 INSERT INTO `accounts`
-(`id`,`name`,`password`,`secret`,`secret_status`,`type`,`premdays`,`coins`,`lastday`,`email`,`creation`,`vote`,`key`,`email_new`,
-`email_new_time`,`rlname`,`location`,`page_access`,`email_code`,`next_email`,`create_date`,`create_ip`,`last_post`,`flag`,
-`guild_points`,`guild_points_stats`,`passed`,`block`,`refresh`,`birth_date`,`gender`,`loyalty_points`,`authToken`)
+(`id`, `name`, `password`, `secret`, `secret_status`, `type`, `premdays`, `coins`, `lastday`, `email`, `creation`, `vote`, `key`, `email_new`,
+ `email_new_time`, `rlname`, `location`, `page_access`, `email_code`, `next_email`, `create_date`, `create_ip`, `last_post`, `flag`,
+ `guild_points`, `guild_points_stats`, `passed`, `block`, `refresh`, `birth_date`, `gender`, `loyalty_points`, `authToken`)
 VALUES
-(2,'sellcharacters',sha1('#@!!@##@!@!#ABCD'),null,0,1,0,0,0,'sell@email.com',0,0,0,0,0,0,'',0,0,0,
-0,0,0,'',0,0,0,0,0,'','m',0,'');
-
+  (2, 'sellcharacters', sha1('#@!!@##@!@!#ABCD'), null, 0, 1, 0, 0, 0, 'sell@email.com', 0, 0, 0, 0, 0, 0, '', 0, 0, 0,
+                                                                                            0, 0, 0, '', 0, 0, 0, 0, 0,
+                                                                                               '', 'm', 0, '');
 
