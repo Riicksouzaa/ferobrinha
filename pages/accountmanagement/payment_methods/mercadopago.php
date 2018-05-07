341957bae66b44062394df496cdbc142d2165869
Ricardo Souza <souzaariick@gmail.com>
2018-05-06 23:58:53 -0300

    opa

--- pages/accountmanagement/payment_methods/mercadopago.php
+++ pages/accountmanagement/payment_methods/mercadopago.php
@@ -10,13 +10,37 @@ try {
     $product_id = $payment_data['ServiceID'];
     if ($config['mp']['sandboxMode']) {
         $mp = new MP($config['mp']['SANDBOX_CLIENT_ID'], $config['mp']['SANDBOX_CLIENT_SECRET']);
+//        MercadoPago\SDK::setClientId($config['mp']['SANDBOX_CLIENT_ID']);
+//        MercadoPago\SDK::setClientSecret($config['mp']['SANDBOX_CLIENT_SECRET']);
     } else {
+//        MercadoPago\SDK::setClientId($config['mp']['CLIENT_ID']);
+//        MercadoPago\SDK::setClientSecret($config['mp']['CLIENT_SECRET']);
         $mp = new MP($config['mp']['CLIENT_ID'], $config['mp']['CLIENT_SECRET']);
     }
-    $mp->sandbox_mode($config['mp']['sandboxMode']);
     $price = (array_keys($config['donate']['offers'][intval($product_id)])[0] / 100);
     $qnt = array_values($config['donate']['offers'][intval($product_id)])[0];
     $purl = $config['base_url'] . 'layouts/tibiacom/images/payment/serviceid_' . ($product_id >= 5 ? '5' : $product_id) . '.png';
+
+//    $mp_http = new MercadoPago\RestClient();
+//    $mp_http->setHttpParam('CURLOPT_SSL_VERIFYPEER', 0);
+//    $mp_http->setHttpParam('CURLOPT_SSL_VERIFYHOST', 0);
+
+
+//    $preference = new MercadoPago\Preference();
+//    $item = new MercadoPago\Item();
+
+//    $item->id = $product_id;
+//    $item->title = $config['sale']['productName'];
+//    $item->quantity = 1;
+//    $item->unit_price = $price;
+
+//    $preference->items = [$item];
+//    $preference->save();
+//    var_dump($preference);
+
+//    $main_content .= "<a href='" . ($config['mp']['sandboxMode'] ? $preference->sandbox_init_point : $preference->init_point) . "'>PAGAR SAPORRA</a>";
+    
+    
     $preference_data = [
         "items" => [
             [
@@ -51,13 +75,13 @@ try {
                         }
                     }
                     </script>
-                    
-                    <a href="' . ($config['mp']['sandboxMode'] == TRUE ? $preference["response"]["sandbox_init_point"] : $preference["response"]["init_point"]) . '" name="MP-Checkout" class="lightblue-Rn-L-Ar-Br" mp-mode="modal" onreturn="execute_my_onreturn">Pagar</a>
+
+                    <a href="' . ($config['mp']['sandboxMode'] == TRUE ? $preference["response"]["sandbox_init_point"] : $preference["response"]["init_point"]) . '" name="MP-Checkout" class="lightblue-Rn-L-Ar-Br" mp-mode="modal" onreturn="execute_my_onreturn()">Pagar</a>
 		            <!--<script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>-->
 		            <!--<script type="text/javascript" src="https://www.mercadopago.com/org-img/jsapi/mptools/buttons/render.js"></script>-->
-		            <script type="text/javascript" src=" https://secure.mlstatic.com/mptools/render.js"></script>
+		            <script type="text/javascript" src="https://secure.mlstatic.com/mptools/render.js"></script>
                 ';
     $main_content .= "</div>";
-} catch (MercadoPagoException $e) {
+} catch (Exception $e) {
     echo $e->getMessage();
 }
