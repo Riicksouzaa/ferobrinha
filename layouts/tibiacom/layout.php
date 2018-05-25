5a7dbe6ee0c1f0228159435e7e266867cdddbae4
Ricardo Souza <souzaariick@gmail.com>
2018-05-24 23:24:01 -0300

    descontinuado

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -680,6 +680,15 @@ if(!defined('INITIALIZED'))
                                     <div class="Border_1" style="background-image:url(layouts/tibiacom/images/global/content/border-1.gif);"></div>
                                     <div class="BorderTitleText" style="background-image:url(layouts/tibiacom/images/global/content/newsheadline_background.gif); height: 28px;">
                                         <div class="InfoBar">
+                                            <?php if(Website::getWebsiteConfig()->getValue('info_bar_cast')){?>
+                                                <a class="InfoBarBlock" href="./" target="_blank">
+                                                    <img class="InfoBarBigLogo" src="layouts/tibiacom/images/global/header/info/icon-cast.png">
+                                                    <span class="InfoBarNumbers"><img class="InfoBarSmallElement" src="layouts/tibiacom/images/global/header/info/icon-streamers.png">
+                                                    <span class="InfoBarSmallElement">0</span><img class="InfoBarSmallElement" src="layouts/tibiacom/images/global/header/info/icon-viewers.png">
+                                                    <span class="InfoBarSmallElement">0</span>
+                                                </span>
+                                                </a>
+                                            <?php }?>
                                             <?php if(Website::getWebsiteConfig()->getValue('info_bar_twitch')){?>
                                             <a class="InfoBarBlock" href="https://www.twitch.tv/directory/game/Tibia" target="_blank">
                                                 <img class="InfoBarBigLogo" src="layouts/tibiacom/images/global/header/info/icon-twitch.png">
