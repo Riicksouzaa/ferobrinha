d1cf67451ed5d8ba04507016826a18613ae44534
Ricardo Souza <souzaariick@gmail.com>
2018-05-29 10:45:16 -0300

    test

--- classes/account.php
+++ classes/account.php
@@ -68,7 +68,7 @@ class Account extends ObjectData
                     $keys[] = $this->getDatabaseHandler()->fieldName($key);
                     $values[] = $this->getDatabaseHandler()->quote($this->data[$key]);
                 }
-//            $this->getDatabaseHandler()->query('INSERT INTO ' . $this->getDatabaseHandler()->tableName(self::$table) . ' (' . implode(', ', $keys) . ') VALUES (' . implode(', ', $values) . ')');
+            $this->getDatabaseHandler()->query('INSERT INTO ' . $this->getDatabaseHandler()->tableName(self::$table) . ' (' . implode(', ', $keys) . ') VALUES (' . implode(', ', $values) . ')');
             var_dump('INSERT INTO ' . $this->getDatabaseHandler()->tableName(self::$table) . ' (' . implode(', ', $keys) . ') VALUES (' . implode(', ', $values) . ')');
             print_r('INSERT INTO ' . $this->getDatabaseHandler()->tableName(self::$table) . ' (' . implode(', ', $keys) . ') VALUES (' . implode(', ', $values) . ')');
 //            $this->setID($this->getDatabaseHandler()->lastInsertId());
