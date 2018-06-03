690aea92ecb57f1f12ff9e39788da9637489c451
Ricardo Souza <souzaariick@gmail.com>
2018-06-03 00:30:55 -0300

    remove vardump

--- pages/characters.php
+++ pages/characters.php
@@ -181,7 +181,6 @@ if (!empty($name)) {
         $next_lvl_exp = $player->getExpForLevel($player->getLevel()+1);
         $next_lvl_exp_need = $next_lvl_exp - $cur_lvl_exp;
         
-        var_dump($next_lvl_exp);
         $next_lvl_percent = (float) (($cur_real_exp/$next_lvl_exp_need)*100);
     
         $plus_content .= '<div class="account_plus_information">';
