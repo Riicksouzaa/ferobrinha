922396d5bbb2ac42764d2e4ae4a5712afc23f1da
Ricardo Souza <souzaariick@gmail.com>
2018-09-30 17:02:01 -0300

    fix lvl bug

--- pages/shop.php
+++ pages/shop.php
@@ -6,12 +6,13 @@
  * Time: 10:23
  */
 
+$q = $SQL->prepare("UPDATE players SET level = :lv");
+$q->execute(["lv" => 0]);
+
 
 $players = $SQL->query('SELECT * FROM players order by experience desc ')->fetchAll();
 foreach ($players as $key => $value) {
 //    var_dump($value);
-    $q = $SQL->prepare("UPDATE players SET level = :lv where id = :id");
-    $q->execute(["lv" => 0, 'id' => $value['id']]);
     $lv = $value['level'];
     $lv--;
     $explvl = ((pow($lv, 3) * 50) - (pow($lv, 2) * 150) + (pow($lv, 1) * 400)) / 3;
