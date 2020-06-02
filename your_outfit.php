3867a032556857441a86fafb0784ac93d3900752
Ricardo Souza <souzaariick@gmail.com>
2020-06-02 13:29:37 -0300

    0 cache

--- your_outfit.php
+++ your_outfit.php
@@ -184,6 +184,7 @@ if (isset($_REQUEST['name'])) {
 
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
     header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
     header('Content-Type: image/png');
     imagepng($im, NULL, 9, PNG_ALL_FILTERS);
     imagedestroy($im);
