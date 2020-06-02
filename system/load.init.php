65873e9a9ec3241fceac23fb5988cb18c4567908
Ricardo Souza <souzaariick@gmail.com>
2020-06-02 11:19:10 -0300

    loki

--- system/load.init.php
+++ system/load.init.php
@@ -147,17 +147,17 @@ $make_double_archs = function ($title) {
 };
 
 // remove magic quotes, to make it compatible with some bad PHP configurations, 'stripslashes' in scripts is not needed anymore!
-if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
-    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
-    while (list($key, $val) = each($process)) {
-        foreach ($val as $k => $v) {
-            unset($process[$key][$k]);
-            if (is_array($v)) {
-                $process[$key][stripslashes($k)] = $v;
-                $process[] = &$process[$key][stripslashes($k)];
-            } else
-                $process[$key][stripslashes($k)] = stripslashes($v);
-        }
-    }
-    unset($process);
-}
+//if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+//    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+//    while (list($key, $val) = each($process)) {
+//        foreach ($val as $k => $v) {
+//            unset($process[$key][$k]);
+//            if (is_array($v)) {
+//                $process[$key][stripslashes($k)] = $v;
+//                $process[] = &$process[$key][stripslashes($k)];
+//            } else
+//                $process[$key][stripslashes($k)] = stripslashes($v);
+//        }
+//    }
+//    unset($process);
+//}
