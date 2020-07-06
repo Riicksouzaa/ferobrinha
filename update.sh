dbd014b9d0a3c3f94560070ceed9b70a9b3f1d60
Ricardo Souza <souzaariick@gmail.com>
2020-07-06 20:43:41 +0000

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#----------------------------------------------/
-#--Git Pull
-#--$ crontab -e
-#--0 * * * * /home/user/repositorio/update.sh
-#----------------------------------------------/
-sudo git pull
