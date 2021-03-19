7d635566e20e4ee348b4342b3e97281584780105
Ricardo Souza <souzaariick@gmail.com>
2021-03-19 14:08:38 -0300

    calendário fix

--- pages/calendario.php
+++ pages/calendario.php
@@ -22,7 +22,7 @@ $main_content .= '
 						<tbody>
 							<tr>
 								<td style="width: 100%;">
-								
+
 			<table style="width: 100%;">
 				<tbody><tr>
 					<td style="width: 100%; text-align: left;">
@@ -31,11 +31,11 @@ $main_content .= '
 						<li>Todos os eventos listados neste calendário são automáticos, portanto não é necessário um Administrador estar online para iniciá-lo.</li>
 						<li>Todos os horários são baseados em <b>UTC/GMT -3 (horário de Brasília).</b></li>
 						<li>Hora e data atualizados:  <b>22:30</b>, <b>23/09/2018</b></li>
-			
+
 					</td>
 				</tr>
 			</tbody></table>
-			
+
 								</td>
 							</tr>
 						</tbody>
@@ -51,43 +51,44 @@ $main_content .= '
 
 
 $main_content .= "<div class='TableContainer'>";
-$main_content .= $make_content_header("Calendário ".$config['server']['serverName']);
+$main_content .= $make_content_header("Calendário " . $config['server']['serverName']);
 $main_content .= $make_table_header('Table3 Table table-responsive');
 $main_content .= '
 <tbody>
     <tr>
-        <td bgcolor="#505050" witdh="'.(100/7).'%"><center><b><font color="white"><small>Segunda-Feira</small></font></b></center></td>
-        <td bgcolor="#505050" witdh="'.(100/7).'%"><center><b><font color="white"><small>Terça-Feira</small></font></b></center></td>
-        <td bgcolor="#505050" witdh="'.(100/7).'%"><center><b><font color="white"><small>Quarta-Feira</small></font></b></center></td>
-        <td bgcolor="#505050" witdh="'.(100/7).'%"><center><b><font color="white"><small>Quinta-Feira</small></font></b></center></td>
-        <td bgcolor="#505050" witdh="'.(100/7).'%"><center><b><font color="white"><small>Sexta-Feira</small></font></b></center></td>
-        <td bgcolor="#505050" witdh="'.(100/7).'%"><center><b><font color="white"><small>Sábado</small></font></b></center></td>
-        <td bgcolor="#505050" witdh="'.(100/7).'%"><center><b><font color="white"><small>Domingo</small></font></b></center></td>
+        <td bgcolor="#505050" witdh="' . (100 / 7) . '%"><center><b><font color="white"><small>Segunda-Feira</small></font></b></center></td>
+        <td bgcolor="#505050" witdh="' . (100 / 7) . '%"><center><b><font color="white"><small>Terça-Feira</small></font></b></center></td>
+        <td bgcolor="#505050" witdh="' . (100 / 7) . '%"><center><b><font color="white"><small>Quarta-Feira</small></font></b></center></td>
+        <td bgcolor="#505050" witdh="' . (100 / 7) . '%"><center><b><font color="white"><small>Quinta-Feira</small></font></b></center></td>
+        <td bgcolor="#505050" witdh="' . (100 / 7) . '%"><center><b><font color="white"><small>Sexta-Feira</small></font></b></center></td>
+        <td bgcolor="#505050" witdh="' . (100 / 7) . '%"><center><b><font color="white"><small>Sábado</small></font></b></center></td>
+        <td bgcolor="#505050" witdh="' . (100 / 7) . '%"><center><b><font color="white"><small>Domingo</small></font></b></center></td>
 	</tr>';
 $event = new Events();
 $events = $event->getEvents();
-//for($i = 0; $i <= 6; $i++){
-    foreach ($events as $name=>$ev){
-        foreach ($ev as $key=>$value){
-            $main_content .='<tr style="max-width: 10px">';
-            for($i = 0; $i <= 6; $i++) {
-                $main_content .= '<td><a href="?subtopic=events&name='.urlencode($value['group']).'">' . $value['name'] . '</a></td>';
+if (!empty($events)) {
+    foreach ($events as $name => $ev) {
+        foreach ($ev as $key => $value) {
+            $main_content .= '<tr style="max-width: 10px">';
+            for ($i = 0; $i <= 6; $i++) {
+                $main_content .= '<td><a href="?subtopic=events&name=' . urlencode($value['group']) . '">' . $value['name'] . '</a></td>';
             }
-            $main_content .='</tr>';
-            $main_content .='<tr style="max-width: 10px">';
-            for($i = 0; $i <= 6; $i++) {
+            $main_content .= '</tr>';
+            $main_content .= '<tr style="max-width: 10px">';
+            for ($i = 0; $i <= 6; $i++) {
                 $main_content .= '<td style="text-align: center">' . $value['time'] . '</td>';
             }
-            $main_content .='</tr>';
-            $main_content .='<tr style="max-width: 10px">';
-            for($i = 0; $i <= 6; $i++) {
+            $main_content .= '</tr>';
+            $main_content .= '<tr style="max-width: 10px">';
+            for ($i = 0; $i <= 6; $i++) {
                 $main_content .= '<td style="text-align: center"> --- </td>';
             }
-            $main_content .='</tr>';
+            $main_content .= '</tr>';
         }
     }
-//}
-$main_content .='</tbody>';
+}
+
+$main_content .= '</tbody>';
 $main_content .= $players_rows;
 $main_content .= $make_table_footer();
 $main_content .= "</div>";
