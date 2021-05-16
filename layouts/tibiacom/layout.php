014166cc6845d962cf898bb393e7bdf45b7fe285
Ricardo Souza <souzaariick@gmail.com>
2021-05-16 15:52:58 -0300

    Kramer bostinha

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -150,7 +150,6 @@ if(!defined('INITIALIZED'))
     <script src="<?php echo $layout_name; ?>/js/ajaxmonteiro.js<?php echo $css_version; ?>"></script>
 <?php } ?>
 <script src="<?php echo $layout_name; ?>/js/iziModal.min.js<?php echo $css_version; ?>"></script>
-<script src="<?php echo $layout_name; ?>/js/bootstrap.min.js<?php echo $css_version; ?>"></script>
 <!--Tiny Editor -->
 <script type="text/javascript" src="./vendor/tinymce/tinymce/tinymce.min.js"></script>
 <!--    <script src='https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=vct6772xi5zrh195yd6scyhdke2ldwlubrpqq024580vr62v'></script>-->
@@ -789,5 +788,15 @@ if ($_REQUEST['subtopic'] == "createaccount") echo '<script src="' . $layout_nam
 <?php }?>
 <?php }?>
    <?php include_once "promo/promo.php"; ?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
