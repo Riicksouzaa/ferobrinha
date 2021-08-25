977523c55bd874039d59b210c27c936b883f7b1b
Ricardo Souza <souzaariick@gmail.com>
2021-08-25 00:57:23 -0300

    endilis

--- endilis.php
+++ endilis.php
@@ -1,1 +1,13 @@
-como cú de curioso
+<?php
+
+$file = __DIR__ . "curioso.json";
+
+$json = json_decode(file_get_contents($file), TRUE);
+
+$newjson = array("cu" => $json["cu"]++, "curioso" => $json["curioso"]++);
+
+file_put_contents($file, json_encode($newjson));
+
+echo $file;
+
+
