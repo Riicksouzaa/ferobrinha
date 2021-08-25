30731307fba4dcdc1a9d58a7e59c9b0f641528f1
Ricardo Souza <souzaariick@gmail.com>
2021-08-25 01:14:43 -0300

    endilis

--- endilis.php
+++ endilis.php
@@ -2,11 +2,12 @@
 
 $file = __DIR__ . "/curioso.json";
 
-$newjson = array("cu" => 0, "curioso" => 0);
-file_put_contents($file, json_encode($newjson));
-
 $json = json_decode(file_get_contents($file), TRUE);
 
-var_dump($newjson, $json, $file);
+$newjson = array("cu" => ++$json["cu"], "curioso" => ++$json["curioso"]);
+
+file_put_contents($file, json_encode($newjson));
+
+echo "Já comi {$json["cu"]} curiosos";
 
 
