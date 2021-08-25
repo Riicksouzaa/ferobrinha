3d6d655fafb5be271dd2034d0e370e8c4a8dadb4
Ricardo Souza <souzaariick@gmail.com>
2021-08-25 01:00:04 -0300

    endilis

--- endilis.php
+++ endilis.php
@@ -8,6 +8,8 @@ $newjson = array("cu" => $json["cu"]++, "curioso" => $json["curioso"]++);
 
 file_put_contents($file, json_encode($newjson));
 
+$json = json_decode(file_get_contents($file), TRUE);
+
 var_dump($json);
 
 
